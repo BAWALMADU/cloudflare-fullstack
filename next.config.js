cf3b3b9968dcf26fe45bed033bbfdd4e6eeca482
BAWALMADU <ikanbawalmadu@gmail.com>
2025-08-20 15:54:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'   // ini penting biar hasil build ke /out
-}
+  output: 'export'
 
 module.exports = nextConfig
